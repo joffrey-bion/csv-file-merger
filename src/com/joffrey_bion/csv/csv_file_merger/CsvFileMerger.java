d3a9efe3d376db06fe2199f2d652f3409e82f2b7
Joffrey BION <joffrey.bion@gmail.com>
2013-06-18 19:29:52 -0400

    better management of the command line

--- src/com/joffrey_bion/csv/csv_file_merger/CsvFileMerger.java
+++ src/com/joffrey_bion/csv/csv_file_merger/CsvFileMerger.java
@@ -6,6 +6,7 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 import com.joffrey_bion.csv.CsvMerger;
+import com.joffrey_bion.file_processor_window.ConsoleLogger;
 import com.joffrey_bion.file_processor_window.JFilePickersPanel;
 import com.joffrey_bion.file_processor_window.JFileProcessorWindow;
 import com.joffrey_bion.file_processor_window.Logger;
@@ -15,12 +16,16 @@ public class CsvFileMerger {
     private static MergerArgsPanel argsPanel;
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                openWindow();
-            }
-        });
+        if (args.length == 0) {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    openWindow();
+                }
+            });
+        } else {
+            mergeFiles(args, null, new ConsoleLogger());
+        }
     }
 
     private static void openWindow() {
@@ -54,15 +59,29 @@ public class CsvFileMerger {
             String[] fileNumbers = argsPanel.getFileNumbers();
             String[] sources = new String[fileNumbers.length];
             String base = getBaseNameWithoutNumber(source);
-            log.println("Sources to merge:");
             for (int i = 0; i < sources.length; i++) {
                 sources[i] = base + fileNumbers[i] + ".csv";
+            }
+            mergeFiles(sources, dest, log);
+        } catch (Exception e) {
+            log.printErr(e.toString());
+        }
+    }
+
+    private static void mergeFiles(String[] sources, String dest, Logger log) {
+        if (sources.length < 2) {
+            log.printErr("Cannot merge less than 2 files!");
+            return;
+        }
+        try {
+            log.println("Sources to merge:");
+            for (int i = 0; i < sources.length; i++) {
                 log.println("> " + sources[i] + "");
             }
             String destFilename;
             if (dest == null || "".equals(dest)) {
                 log.println("No output file selected.");
-                destFilename = generateDestFilename(source);
+                destFilename = generateDestFilename(sources[0]);
                 log.println("Auto output filename: " + destFilename);
             } else {
                 destFilename = dest;
@@ -71,19 +90,21 @@ public class CsvFileMerger {
 
             CsvMerger merger = new CsvMerger(destFilename);
             merger.merge(sources);
-
             log.println("Success.");
         } catch (IOException e) {
             e.printStackTrace();
             log.printErr(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-            log.printErr(e.toString());
         }
     }
 
     private static String generateDestFilename(String source) {
-        return getBaseNameWithoutNumber(source) + "-merged.csv";
+        String base = getBaseNameWithoutNumber(source);
+        char lastChar = base.charAt(base.length() - 1);
+        if (lastChar == '-' || lastChar == '_') {
+            return base + "merged.csv";
+        } else {
+            return base + "-merged.csv";
+        }
     }
 
     private static String getBaseNameWithoutNumber(String fullName) {

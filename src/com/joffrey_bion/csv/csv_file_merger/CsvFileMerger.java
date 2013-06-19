b0c0ab952f9bf0a7c195c90f4f1be9afb1deb5d5
Joffrey BION <joffrey.bion@gmail.com>
2013-06-19 14:53:34 -0400

    Added documentation

--- src/com/joffrey_bion/csv/csv_file_merger/CsvFileMerger.java
+++ src/com/joffrey_bion/csv/csv_file_merger/CsvFileMerger.java
@@ -1,6 +1,7 @@
 package com.joffrey_bion.csv.csv_file_merger;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -11,10 +12,26 @@ import com.joffrey_bion.file_processor_window.JFilePickersPanel;
 import com.joffrey_bion.file_processor_window.JFileProcessorWindow;
 import com.joffrey_bion.file_processor_window.Logger;
 
+/**
+ * This program merges two or more CSV files into one. The different sources must
+ * have the same column headers.
+ * 
+ * @author <a href="mailto:joffrey.bion@gmail.com">Joffrey BION</a>
+ */
 public class CsvFileMerger {
+
+    private static final int ARG_SOURCES_START = 1;
+    private static final int ARG_DEST = 0;
 
     private static MergerArgsPanel argsPanel;
 
+    /**
+     * Choose between GUI or console version according to the number of arguments.
+     * 
+     * @param args
+     *            No arguments will start the GUI, otherwise filenames have to be
+     *            specified: destination first, then all the sources.
+     */
     public static void main(String[] args) {
         if (args.length == 0) {
             SwingUtilities.invokeLater(new Runnable() {
@@ -23,11 +40,17 @@ public class CsvFileMerger {
                     openWindow();
                 }
             });
+        } else if (args.length >= 3) {
+            mergeFiles(Arrays.copyOfRange(args, ARG_SOURCES_START, args.length), args[ARG_DEST],
+                    new ConsoleLogger());
         } else {
-            mergeFiles(args, null, new ConsoleLogger());
+            System.out.println("Usage: CsvFileMerger <dest> <source1> <source2> [source3]*");
         }
     }
 
+    /**
+     * Starts the GUI.
+     */
     private static void openWindow() {
         // windows system look and feel for the window
         try {
@@ -43,13 +66,24 @@ public class CsvFileMerger {
                 argsPanel) {
             @Override
             public void process(String[] inPaths, String[] outPaths) {
-                processFile(inPaths[0], outPaths[0], this);
+                processNumberedFiles(inPaths[0], outPaths[0], this);
             }
         };
         frame.setVisible(true);
     }
 
-    private static void processFile(String source, String dest, Logger log) {
+    /**
+     * Calls {@link #mergeFiles(String[], String, Logger)} with a list of numbered
+     * filenames as the sources.
+     * 
+     * @param source
+     *            The filename of the first source containing the first number.
+     * @param dest
+     *            The destination filename.
+     * @param log
+     *            A {@link Logger} to display log messages.
+     */
+    private static void processNumberedFiles(String source, String dest, Logger log) {
         log.clearLog();
         if (source == null || "".equals(source)) {
             log.printErr("No input file pattern selected.");
@@ -68,7 +102,17 @@ public class CsvFileMerger {
         }
     }
 
-    private static void mergeFiles(String[] sources, String dest, Logger log) {
+    /**
+     * Merges the given source files into the given destination file.
+     * 
+     * @param sources
+     *            An array of filenames referring to the sources.
+     * @param destination
+     *            The destination filename.
+     * @param log
+     *            A {@link Logger} to display log messages.
+     */
+    private static void mergeFiles(String[] sources, String destination, Logger log) {
         if (sources.length < 2) {
             log.printErr("Cannot merge less than 2 files!");
             return;
@@ -79,12 +123,12 @@ public class CsvFileMerger {
                 log.println("> " + sources[i] + "");
             }
             String destFilename;
-            if (dest == null || "".equals(dest)) {
+            if (destination == null || "".equals(destination)) {
                 log.println("No output file selected.");
                 destFilename = generateDestFilename(sources[0]);
                 log.println("Auto output filename: " + destFilename);
             } else {
-                destFilename = dest;
+                destFilename = destination;
             }
             log.println("Processing...");
 
@@ -97,6 +141,14 @@ public class CsvFileMerger {
         }
     }
 
+    /**
+     * Returns a destination filename based on the name of the specified source,
+     * removing its number.
+     * 
+     * @param source
+     *            The source numbered filename
+     * @return The generated destination filename.
+     */
     private static String generateDestFilename(String source) {
         String base = getBaseNameWithoutNumber(source);
         char lastChar = base.charAt(base.length() - 1);
@@ -107,6 +159,13 @@ public class CsvFileMerger {
         }
     }
 
+    /**
+     * Removes the 3-digit number and the extension of a CSV filename.
+     * 
+     * @param fullName
+     *            The initial filename with the number and the extension.
+     * @return The truncated filename without number and extension.
+     */
     private static String getBaseNameWithoutNumber(String fullName) {
         int extPosition = fullName.lastIndexOf(".");
         if (extPosition == -1) {
